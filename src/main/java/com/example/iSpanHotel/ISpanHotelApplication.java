2fecf525a4532b71685b78bad4cc707dca38cd2f
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-14 15:58:40 +0800

    init

---
+++ src/main/java/com/example/iSpanHotel/ISpanHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.iSpanHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISpanHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISpanHotelApplication.class, args);
+	}
+
+}
