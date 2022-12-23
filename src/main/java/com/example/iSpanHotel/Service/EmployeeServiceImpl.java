c7db5ec8ecdea448ddafd469d7ca611ca1262fea
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-23 11:15:26 +0800

    Create EmployeeServiceImpl.java

---
+++ src/main/java/com/example/iSpanHotel/Service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.iSpanHotel.Service;
+
+import javax.servlet.http.HttpSession;
+
+import com.example.iSpanHotel.Dto.EmployeeDto;
+
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	@Override
+	public String create(EmployeeDto employeeDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(EmployeeDto employeeDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(EmployeeDto employeeDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findAll(EmployeeDto employeeDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findById(EmployeeDto employeeDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String login(HttpSession session, String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
