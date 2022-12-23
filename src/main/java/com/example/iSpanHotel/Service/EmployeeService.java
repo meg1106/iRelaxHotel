96c1dd6019bbdc19704d489f2d7a72dc4fef3e33
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-23 11:15:23 +0800

    Create EmployeeService.java

---
+++ src/main/java/com/example/iSpanHotel/Service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.example.iSpanHotel.Service;
+
+import javax.servlet.http.HttpSession;
+
+import com.example.iSpanHotel.Dto.EmployeeDto;
+
+
+public interface EmployeeService {
+	
+	// 創建員工帳號
+	String create(EmployeeDto employeeDto);
+	
+	// 刪除員工帳號
+	String delete(EmployeeDto employeeDto);
+	
+	// 修改員工帳號
+	String update(EmployeeDto employeeDto);
+	
+	// 查詢所有員工帳號
+	String findAll(EmployeeDto employeeDto);
+	
+	// 根據id查詢員工帳號
+	String findById(EmployeeDto employeeDto);
+	
+	// 員工帳號登入 
+	String login(HttpSession session,String username,String password);
+	
+}
