5579685fa93af066f3425914572ced0fc3bae919
VikingFxx <nienfxxq@gmail.com>
2022-12-26 11:28:55 +0800

    Create Member.java

---
+++ src/main/java/com/example/iSpanHotel/model/Member.java
@@ -0,0 +1,87 @@
+package com.example.iSpanHotel.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class Member {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "account", nullable = false, unique = true)
+	private String account;
+	
+	@Column(name = "passwd", nullable = false, unique = false)
+	private String passwd;
+	
+	@Column(name = "realname", nullable = false, unique = false)
+	private String realName;
+	
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "tel", nullable = false, unique = true)
+	private String tel;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", account=" + account + ", passwd=" + passwd + 
+					", realName=" + realName + ", email=" + email + ", tel=" + tel +"]";
+	}
+}
