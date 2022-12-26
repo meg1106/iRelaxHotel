d5dc5f97aeb31c7ceff669a7ab45c0534efa9ac5
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-26 13:52:16 +0800

    Update RoomType.java

--- src/main/java/com/example/iSpanHotel/model/RoomType.java
+++ src/main/java/com/example/iSpanHotel/model/RoomType.java
@@ -1,10 +1,14 @@
 package com.example.iSpanHotel.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,4 +30,72 @@ public class RoomType {
 	
 	@Column(name = "content", nullable = true, unique = false)
 	private String content;
+	
+	@OneToMany(mappedBy = "roomType", cascade = CascadeType.ALL)
+	private List<RoomService> roomServices;
+	
+	@OneToMany(mappedBy = "roomType", cascade = CascadeType.ALL)
+	private List<RoomOrder> roomOrders;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRoomtype() {
+		return roomtype;
+	}
+
+	public void setRoomtype(String roomtype) {
+		this.roomtype = roomtype;
+	}
+
+	public Integer getRoomprice() {
+		return roomprice;
+	}
+
+	public void setRoomprice(Integer roomprice) {
+		this.roomprice = roomprice;
+	}
+
+	public String getRoompic() {
+		return roompic;
+	}
+
+	public void setRoompic(String roompic) {
+		this.roompic = roompic;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<RoomService> getRoomServices() {
+		return roomServices;
+	}
+
+	public void setRoomServices(List<RoomService> roomServices) {
+		this.roomServices = roomServices;
+	}
+
+	public List<RoomOrder> getRoomOrders() {
+		return roomOrders;
+	}
+
+	public void setRoomOrders(List<RoomOrder> roomOrders) {
+		this.roomOrders = roomOrders;
+	}
+
+	@Override
+	public String toString() {
+		return "RoomType [id=" + id + ", roomtype=" + roomtype + ", roomprice=" + roomprice + ", roompic=" + roompic
+				+ ", content=" + content + ", roomServices=" + roomServices + "]";
+	}
 }
