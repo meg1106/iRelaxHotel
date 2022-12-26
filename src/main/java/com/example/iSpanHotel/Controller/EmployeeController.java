3e15da707350c8a8959360a636fe13cc4931595b
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-26 11:32:04 +0800

    Update EmployeeController.java

--- src/main/java/com/example/iSpanHotel/Controller/EmployeeController.java
+++ src/main/java/com/example/iSpanHotel/Controller/EmployeeController.java
@@ -1,22 +1,25 @@
 package com.example.iSpanHotel.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.iSpanHotel.Dto.EmployeeDto;
+import com.example.iSpanHotel.Service.EmployeeService;
 
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
 	
 	@PostMapping("/create")
 	private String create(@RequestBody EmployeeDto employeeDto) {
-		
-		
+
 		return "成功";
 	}
 	
