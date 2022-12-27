95be12074d3e96463f53583ac55f5cb51a4da537
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-27 11:26:34 +0800

    Create HotelNewsDto.java

---
+++ src/main/java/com/example/iSpanHotel/Dto/HotelNewsDto.java
@@ -0,0 +1,60 @@
+package com.example.iSpanHotel.Dto;
+
+public class HotelNewsDto {
+	private Long id;
+	private short newsType;
+	private String title;
+	private String startDate;
+	private String endDate;
+	private String pic;
+	private String content;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public short getNewsType() {
+		return newsType;
+	}
+	public void setNewsType(short newsType) {
+		this.newsType = newsType;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "HotelNewsDto [id=" + id + ", newsType=" + newsType + ", title=" + title + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", pic=" + pic + ", content=" + content + "]";
+	}
+	
+}
