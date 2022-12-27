7cdee072e6ac6e36aefc52022c6e5bd1ec1836be
gavinlin123 <gavinlin123@yahoo.com.tw>
2022-12-27 11:26:32 +0800

    Create HotelNewsDao.java

---
+++ src/main/java/com/example/iSpanHotel/Dao/HotelNewsDao.java
@@ -0,0 +1,9 @@
+package com.example.iSpanHotel.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.iSpanHotel.model.HotelNews;
+
+public interface HotelNewsDao extends JpaRepository<HotelNews, Long> {
+
+}
